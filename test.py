5ed457b21de6d3fd91ceb570bacc9acfef6c9ac0
Ha Nguyen <hanguyen.0096@gmail.com>
2020-11-30 21:21:59 -0500

    add filter

--- test.py
+++ test.py
@@ -20,7 +20,6 @@ from lime import lime_text
 import gensim
 from gensim.models import Word2Vec
 from sklearn.decomposition import PCA
-from matplotlib import pyplot
 
 
 import gensim.downloader as gensim_api
@@ -33,11 +32,19 @@ from tensorflow.keras import backend as K
 data = []
 with open('jobs.json', mode='r', errors='ignore') as json_file:
     data = json.load(json_file)
-
 dtf = pd.DataFrame(data)
 
-dtf = dtf[dtf["title"].isin(['Software Engineer', 'Business Development Manager', 'Product Manager'])][["title", "job_description"]]
+dtf=dtf[["title","job_description"]]
+search_for_product_manager=['Product Manager','Warehouse']
+dtf.loc[dtf['title'].str.contains('|'.join(search_for_product_manager), case=False), 'title'] = 'Product Manager'
+search_for_business_development_manager=['Sales','Business Development Manager']
+dtf.loc[dtf['title'].str.contains('|'.join(search_for_business_development_manager), case=False), 'title'] = 'Business Development Manager'
+search_for_software_engineer=['Full Stack','Software Engineer','Engineer','Developer']
+dtf.loc[dtf['title'].str.contains('|'.join(search_for_software_engineer), case=False), 'title'] = 'Software Engineer'
+
+dtf = dtf[dtf["title"].isin(['Software Engineer','Business Development Manager','Product Manager']) ][["title","job_description"]]
 dtf = dtf.rename(columns={"title": "y", "job_description": "text"})
+
 
 # fig, ax = plt.subplots()
 # fig.suptitle("y", fontsize=12)
@@ -73,10 +80,14 @@ def utils_preprocess_text(text, flg_stemm = False, flg_lemm = True, lst_stopword
 lst_stopwords = nltk.corpus.stopwords.words("english")
 
 
-dtf["text_clean"] = dtf["text"].apply(lambda x: utils_preprocess_text(x, flg_stemm=False, flg_lemm=True, lst_stopwords=lst_stopwords))
-y_train = dtf["y"].values
+dtf["text_clean"] = dtf["text"].apply(lambda x: utils_preprocess_text(x, flg_stemm=False, flg_lemm=False, lst_stopwords=lst_stopwords))
+# split dataset
+dtf_train, dtf_test = model_selection.train_test_split(dtf, test_size=0.3)
+# get target
+y_train = dtf_train["y"].values
+y_test = dtf_test["y"].values
 
-corpus = dtf["text_clean"]
+corpus = dtf_train["text_clean"]
 
 # create list of lists of unigrams
 lst_corpus = []
@@ -91,7 +102,7 @@ bigrams_detector = gensim.models.phrases.Phraser(bigrams_detector)
 trigrams_detector = gensim.models.phrases.Phrases(bigrams_detector[lst_corpus], delimiter=" ".encode(), min_count=5, threshold=10)
 trigrams_detector = gensim.models.phrases.Phraser(trigrams_detector)
 
-nlp = gensim.models.word2vec.Word2Vec(lst_corpus, size=300, window=8, min_count=1, sg=1, iter=30)
+nlp = gensim.models.word2vec.Word2Vec(lst_corpus, size=300, window=155, min_count=1, sg=1, iter=30)
 
 words = list(nlp.wv.vocab)
 # fit a 2d PCA model to the vectors
@@ -112,7 +123,7 @@ result = pca.fit_transform(X)
 #
 # plt.show()
 
-text_len = 100
+text_len = 50
 # tokenize text
 tokenizer = kprocessing.text.Tokenizer(lower=True, split=' ', oov_token="NaN", filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n')
 tokenizer.fit_on_texts(lst_corpus)
@@ -121,9 +132,29 @@ dic_vocabulary = tokenizer.word_index
 lst_text2seq= tokenizer.texts_to_sequences(lst_corpus)
 # padding sequence
 X_train = kprocessing.sequence.pad_sequences(lst_text2seq, maxlen=text_len, padding="post", truncating="post")
+#
+sns.heatmap(X_train==0, vmin=0, vmax=1, cbar=False).set_title('Feature matrix')
+plt.xlabel("Sequence length")
+plt.ylabel("Number of sequence")
+plt.show()
 
-sns.heatmap(X_train==0, vmin=0, vmax=1, cbar=False)
-# plt.show()
+corpus = dtf_test["text_clean"]
+
+# create list of n-grams
+lst_corpus = []
+for string in corpus:
+    lst_words = string.split()
+    lst_grams = [" ".join(lst_words[i:i+1]) for i in range(0, len(lst_words), 1)]
+    lst_corpus.append(lst_grams)
+
+# detect common bigrams and trigrams using the fitted detectors
+lst_corpus = list(bigrams_detector[lst_corpus])
+lst_corpus = list(trigrams_detector[lst_corpus])
+# text to sequence with the fitted tokenizer
+lst_text2seq = tokenizer.texts_to_sequences(lst_corpus)
+
+# padding sequence
+X_test = kprocessing.sequence.pad_sequences(lst_text2seq, maxlen=text_len, padding="post", truncating="post")
 
 # start the matrix (length of vocabulary x vector size) with all 0s
 embeddings = np.zeros((len(dic_vocabulary)+1, 300))
@@ -194,3 +225,62 @@ for metric in metrics:
     ax22.plot(training.history['val_'+metric], label=metric)
 ax22.set_ylabel("Score", color="steelblue")
 plt.show()
+
+
+predicted_prob = model.predict(X_test)
+predicted = [dic_y_mapping[np.argmax(pred)] for pred in predicted_prob]
+
+# select observation
+i = 0
+txt_instance = str(dtf_test["text"].iloc[i])
+# check true value and predicted value
+print("True:", y_train[i], "--> Pred:", predicted[i], "| Prob:", round(np.max(predicted_prob[i]),2))
+
+# show explanation
+# 1. preprocess input
+# lst_corpus = []
+# for string in [re.sub(r'[^\w\s]','', txt_instance.lower().strip())]:
+#     lst_words = string.split()
+#     lst_grams = [" ".join(lst_words[i:i+1]) for i in range(0,
+#                                                            len(lst_words), 1)]
+#     lst_corpus.append(lst_grams)
+# lst_corpus = list(bigrams_detector[lst_corpus])
+# lst_corpus = list(trigrams_detector[lst_corpus])
+# X_instance = kprocessing.sequence.pad_sequences(
+#     tokenizer.texts_to_sequences(corpus), maxlen=text_len,
+#     padding="post", truncating="post")
+# # 2. get attention weights
+# layer = [layer for layer in model.layers if "attention" in
+#          layer.name][0]
+# func = K.function([model.input], [layer.output])
+# weights = func(X_instance)[0]
+# weights = np.mean(weights, axis=2).flatten()
+# # 3. rescale weights, remove null vector, map word-weight
+# weights = preprocessing.MinMaxScaler(feature_range=(0, 1)).fit_transform(np.array(weights).reshape(-1, 1)).reshape(-1)
+# weights = [weights[n] for n,idx in enumerate(X_instance[0]) if idx != 0]
+# dic_word_weigth = {word: weights[n] for n,word in
+#                    enumerate(lst_corpus[0]) if word in
+#                    tokenizer.word_index.keys()}
+# # 4. barplot
+# if len(dic_word_weigth) > 0:
+#     dtf = pd.DataFrame.from_dict(dic_word_weigth, orient='index',
+#                                  columns=["score"])
+#     dtf.sort_values(by="score",
+#                     ascending=True).tail(top).plot(kind="barh",
+#                                                    legend=False).grid(axis='x')
+#     plt.show()
+# else:
+#     print("--- No word recognized ---")
+# # 5. produce html visualization
+# text = []
+# for word in lst_corpus[0]:
+#     weight = dic_word_weigth.get(word)
+#     if weight is not None:
+#         text.append('<b><span style="background-color:rgba(100,149,237,' + str(weight) + ');">' + word + '</span></b>')
+#     else:
+#         text.append(word)
+# text = ' '.join(text)
+# # 6. visualize on notebook
+# print("\033[1m"+"Text with highlighted words")
+# # from IPython.core.display import display, HTML
+# # display(HTML(text))
